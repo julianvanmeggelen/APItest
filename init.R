f791ead9a49f7a5b7af36d5d8121df16fea62959
julianvanmeggelen <60846634+julianvanmeggelen@users.noreply.github.com>
2020-02-09 16:48:04 +0100

    Update init.R

--- init.R
+++ init.R
@@ -4,7 +4,7 @@
 #
 ###########################################################
 # Update this line with the R packages to install:
-my_packages = c("plumber")
+my_packages = c("quantmod")
 ###########################################################
 install_if_missing = function(p) {
 if (p %in% rownames(installed.packages()) == FALSE) {
